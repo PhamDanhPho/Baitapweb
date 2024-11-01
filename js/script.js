992ae212341a9f98a95faa62f3e55c3121df3d60
oigioi <nguyentuandat0101@gmail.com>
2024-11-01 22:29:49 +0700

    Update script.js

--- js/script.js
+++ js/script.js
@@ -9,7 +9,7 @@ const visibleItems = 3; // Số sản phẩm hiển thị cùng một lúc
 
 // Cập nhật băng chuyền
 function updateCarousel() {
-    const itemWidth = items[0].getBoundingClientRect().width; // Kích thước của sản phẩm
+    const itemWidth = items[0].getBoundingClientRect().width+15; // Kích thước của sản phẩm
     const offset = -currentIndex * (itemWidth / visibleItems); // Tính toán khoảng cách dịch chuyển
     track.style.transform = `translateX(${offset}px)`; // Áp dụng dịch chuyển
 }
@@ -25,7 +25,6 @@ nextButton.addEventListener('click', () => {
 
 // Nhấn nút "trước"
 prevButton.addEventListener('click', () => {
-    console.log(currentIndex)
     currentIndex -= visibleItems; // Giảm chỉ số hiện tại
     if (currentIndex < 0) {
         currentIndex = totalItems + visibleItems+1; // Nếu nhỏ hơn 0, quay lại sản phẩm cuối cùng
@@ -35,7 +34,6 @@ prevButton.addEventListener('click', () => {
 
 // Tự động chuyển đổi mỗi 4 giây
 setInterval(() => {
-    console.log(currentIndex)
     currentIndex += visibleItems; // Tăng chỉ số hiện tại
     if (currentIndex >= totalItems+8) {
         currentIndex = 0; // Quay lại sản phẩm đầu tiên
